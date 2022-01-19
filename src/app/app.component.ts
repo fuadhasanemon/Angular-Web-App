24661aabca4fe7b7fa908be41d23e0ba1f7b1aad
fuadhasanemon <fuadhasanemon8@gmail.com>
2022-01-20 00:20:47 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  userName = 'Fuad';
+
+  addLanguage () {
+    console.log("addLanguage function called")
+  }
 }
